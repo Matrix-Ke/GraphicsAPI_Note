127e34716b11c49529ffe1eafb87d268aa32e43e
Matrix <2513084541@qq.com>
2022-06-29 17:32:58 +0800

    【Vulkan】： add  submodule (#1)
    * vulkan  presentation
    
    * add cmakeinit submodule

---
+++ Vulkan/Source/01_Presentation/06_swap_chain_creation/main.cpp
@@ -0,0 +1,20 @@
+#include "swap_chain_creation.h"
+
+
+int main()
+{
+	HelloTriangleApplication app;
+
+	try
+	{
+		app.run();
+	}
+
+	catch (const std::exception& e)
+	{
+		std::cerr << e.what() << std::endl;
+		return EXIT_FAILURE;
+	}
+
+	return EXIT_SUCCESS;
+}
