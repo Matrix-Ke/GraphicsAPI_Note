e8568a1c2cfb889b15d82a9bc351ac172c527262
Matrix <2513084541@qq.com>
2022-07-11 01:59:21 +0800

    model loading

---
+++ Source/Vulkan/Source/08_Model_Loading/28_Model_Loading/main.cpp
@@ -0,0 +1,20 @@
+#include "Model_Loading.h"
+
+
+int main()
+{
+	HelloTriangleApplication app;
+
+	try
+	{
+		app.run();
+	}
+
+	catch (const std::exception& e)
+	{
+		std::cerr << e.what() << std::endl;
+		return EXIT_FAILURE;
+	}
+
+	return EXIT_SUCCESS;
+}
