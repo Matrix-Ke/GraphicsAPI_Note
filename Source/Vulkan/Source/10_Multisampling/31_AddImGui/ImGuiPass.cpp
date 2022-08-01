e9891653c65286c35924cc8df2f61fd98aeba2a6
Matrix <2513084541@qq.com>
2022-08-01 14:33:29 +0800

    Add  ImGui (#11)
    Add  ImGui

---
+++ Source/Vulkan/Source/10_Multisampling/31_AddImGui/ImGuiPass.cpp
@@ -0,0 +1,23 @@
+#include "ImGuiPass.h"
+
+void check_vk_result(VkResult err)
+{
+	if (err == 0)
+		return;
+	fprintf(stderr, "[vulkan] Error: VkResult = %d\n", err);
+	if (err < 0)
+		abort();
+}
+
+VkAllocationCallbacks* VulkanSetup::g_Allocator = NULL;
+VkInstance					VulkanSetup::g_Instance = VK_NULL_HANDLE;
+VkPhysicalDevice			VulkanSetup::g_PhysicalDevice = VK_NULL_HANDLE;
+VkDevice					VulkanSetup::g_Device = VK_NULL_HANDLE;
+uint32_t					VulkanSetup::g_QueueFamily = (uint32_t)-1;
+VkQueue						VulkanSetup::g_Queue = VK_NULL_HANDLE;
+VkDebugReportCallbackEXT	VulkanSetup::g_DebugReport = VK_NULL_HANDLE;
+VkPipelineCache				VulkanSetup::g_PipelineCache = VK_NULL_HANDLE;
+VkDescriptorPool			VulkanSetup::g_DescriptorPool = VK_NULL_HANDLE;
+ImGui_ImplVulkanH_Window	VulkanSetup::g_MainWindowData;
+int							VulkanSetup::g_MinImageCount = 2;
+bool						VulkanSetup::g_SwapChainRebuild = false;
